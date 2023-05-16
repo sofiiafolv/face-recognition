d4b39a227cbb07d9aee0738999079febc5cf0ad7
OlesiaOmelchuk <olesyao2003@gmail.com>
2023-05-16 19:51:21 +0300

    added python modules for svd and svd_qr

---
+++ svd_qr.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def simultaneous_power_iteration(A, k):
+    n, m = A.shape
+    Q = np.random.rand(n, k)
+    Q, _ = np.linalg.qr(Q)
+    Q_prev = Q
+    for _ in range(1000):
+        Z = A.dot(Q)
+        Q, R = np.linalg.qr(Z)
+        err = ((Q - Q_prev) ** 2).sum()
+        Q_prev = Q
+        if err < 1e-3:
+            break
+    return np.diag(R), Q
+
+
+def reduced_svd_using_qr(A):
+    eigenvalues, V = simultaneous_power_iteration(A.T @ A, 200)
+    sigma = np.sqrt(eigenvalues)
+    idx = np.argsort(eigenvalues)[::-1]
+    sigma = sigma[idx]
+    rank_of_A = np.linalg.matrix_rank(A)
+    V = V[:, idx]
+    sigma = sigma[:rank_of_A]
+    V = V[:, :rank_of_A]
+    U = (A @ V) @ np.linalg.inv(np.diag(sigma))
+    return U, sigma, V.T
